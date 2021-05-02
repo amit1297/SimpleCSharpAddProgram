76e6dfe1fe4e8309b93e1099460e8a9b6349af20
Amit Kumar <amit1561992@gmail.com>
2021-05-02 17:00:21 +0530

    add program in c sharp

---
+++ SimpleCSharpAddProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleCSharpAddProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number: ");
+            int a  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+
+            Console.WriteLine("=================================");
+            Console.WriteLine("Your result is " + c);
+            Console.WriteLine("=================================");
+            Console.ReadLine();
+        }
+    }
+}
